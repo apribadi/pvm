f45b2dac369762d8a23d2bbee6a1dfa258f1f256
Aaron Pribadi <aaron.pribadi@gmail.com>
2025-04-01 18:43:23 -0700

    z

--- render.c
+++ render.c
@@ -129,7 +129,6 @@ static size_t op_ge_imm(Env * env, Ins * code, struct Tbl * tbl, size_t ip, Ins 
   return DISPATCH(env, code, tbl, ip + 1);
 }
 
-
 static size_t op_and(Env * env, Ins * code, struct Tbl * tbl, size_t ip, Ins ins) {
   uint8x16x4_t x = vld1q_u8_x4((uint8_t *) env->v[ins.and.x]);
   uint8x16x4_t y = vld1q_u8_x4((uint8_t *) env->v[ins.and.y]);
@@ -165,28 +164,48 @@ static struct Tbl TBL = {
 };
 
 void render(Env env[NUM_THREADS], Ins * code, uint8_t image[RESOLUTION][RESOLUTION]) {
-  float side = 2.0;
-  float xmin = -1.0;
-  float ymin = -1.0;
-  float ymax = ymin + side;
-  float step = side / RESOLUTION;
-  float half = step * 0.5f;
+  float side = 2.0f;
+  float step = side /  RESOLUTION;
+  float half = side /  RESOLUTION / 2.0f;
+  float xmin0 = -1.0f;
+  float ymax0 = 1.0f;
+  uint8_t * p0 = (uint8_t *) image;
 
 #pragma omp parallel for num_threads(NUM_THREADS)
-  for (size_t i = 0; i < RESOLUTION; i += 4) {
-    Env * tenv = &env[omp_get_thread_num()];
-    float y = ymax - half - step * (float) i;
-    for (size_t k = 0; k < 4; k ++) {
-      tenv->y[k] = y - step * (float) k;
-    }
-    for (size_t j = 0; j < RESOLUTION; j += 16) {
-      float x = xmin + half + step * (float) j;
-      for (size_t k = 0; k < 16; k ++) {
-        tenv->x[k] = x + step * (float) k;
-      }
-      size_t result = DISPATCH(tenv, code, &TBL, 0);
-      for (size_t k = 0; k < 4; k ++) {
-        memcpy(&image[i + k][j], &tenv->v[result][4 * k], 16);
+  for (size_t t1 = 0; t1 < 16; t1 ++) {
+    Env * e = &env[omp_get_thread_num()];
+    size_t i1 = t1 & 3;
+    size_t j1 = t1 >> 2;
+    float xmin1 = xmin0 + (side / 4.0f) * (float) i1;
+    float ymax1 = ymax0 - (side / 4.0f) * (float) j1;
+    uint8_t * p1 = p0 + (RESOLUTION / 4) * i1 + (RESOLUTION / 4) * RESOLUTION * j1;
+    for (size_t t2 = 0; t2 < 16; t2 ++) {
+      size_t i2 = t2 & 3;
+      size_t j2 = t2 >> 2;
+      float xmin2 = xmin1 + (side / 16.0f) * (float) i2;
+      float ymax2 = ymax1 - (side / 16.0f) * (float) j2;
+      uint8_t * p2 = p1 + (RESOLUTION / 16) * i2 + (RESOLUTION / 16) * RESOLUTION * j2;
+      for (size_t t3 = 0; t3 < 16; t3 ++) {
+        size_t i3 = t3 & 3;
+        size_t j3 = t3 >> 2;
+        float xmin3 = xmin2 + (side / 64.0f) * (float) i3;
+        float ymax3 = ymax2 - (side / 64.0f) * (float) j3;
+        uint8_t * p3 = p2 + (RESOLUTION / 64) * i3 + (RESOLUTION / 64) * RESOLUTION * j3;
+        for (size_t k = 0; k < 16; k ++) {
+          e->x[k] = (xmin3 + half) + step * (float) k;
+        }
+        for (size_t t4 = 0; t4 < 4; t4 ++) {
+          float ymax4 = ymax3 - (side / 256.0f) * (float) t4;
+          uint8_t * p4 = p3 + (RESOLUTION / 256) * RESOLUTION * t4;
+          for (size_t k = 0; k < 4; k ++) {
+            e->y[k] = (ymax4 - half) - step * (float) k;
+          }
+          size_t result = DISPATCH(e, code, &TBL, 0);
+          for (size_t k = 0; k < 4; k ++) {
+            uint8_t * p5 = p4 + RESOLUTION * k;
+            memcpy(p5, &e->v[result][4 * k], 16);
+          }
+        }
       }
     }
   }
